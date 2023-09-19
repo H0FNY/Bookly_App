898e7b7359a48724b3039684ff06fa72212e5de7
H0FNY <ahofny8@gmail.com>
2023-09-19 20:40:05 +0300

    search and display data from Api done

---
+++ lib/Features/search/data/repos/search_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/search/data/repos/search_repo.dart';
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImpl implements SearchRepo {
+  final ApiService apiService;
+  SearchRepoImpl(this.apiService);
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBooks({required String search}) async {
+    try {
+      var data = await apiService.get(
+          endPoint: "volumes?Filtering=free-ebooks&Sorting=newest&q=$search");
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+}
